3b9be661085b6b7cedc296f51694928ea2172c14
Abayomi Aishida <yomicorperate@gmail.com>
2020-11-19 02:47:41 +0100

    Creating state for image linds

---
+++ src/components/ImageCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Img from "./_MG_9800.jpg";
+
+function ImageCard() {
+  const state = {
+    men: [
+      {
+        id: 1,
+        imgLink: "https://i.ibb.co/R70vBrQ/men.png",
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={state.men.imgLink} className="card-img-top" alt="..." />
+        <p>This is some paragraph</p>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ImageCard;
